6b6ff1ea5f66c86d409c396c065c14f2f8d13d4c
soniathomass <soniathomas239@gmail.com>
2019-05-16 15:54:55 +0530

    first commit

---
+++ arms.sh
@@ -0,0 +1,21 @@
+
+echo "Enter the number"
+read n
+t=$n
+s=0
+b=0
+c=10
+while [ $n -gt $b ]
+do
+r=`expr $n % $c`
+i=`expr $r \* $r \* $r`
+s=`expr $s + $i`
+n=`expr $n / $c`
+done
+echo "sum of cube of digits of number = $s"
+if [ $s -eq $t ]
+then
+echo "$t Amstrong number"
+else
+echo "$t Not an Armstrong number"
+fi
